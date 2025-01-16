891acc214f580b418b819cbc744a756f5c5035af
yukseltron <h.yuksel04@gmail.com>
2025-01-16 05:09:51 -0500

    8

--- 2025/8/sketch.js
+++ 2025/8/sketch.js
@@ -1,31 +1,50 @@
-//draw 1 million little dots of various colors and have them animated by "smearing" them in a random direction
-//
-
-let dots = [];
-let dotCount = 1000;
+let circles = [];
+let i = 0;
+let x = 0;
+let y = 0;
 
 function setup() {
-    createCanvas(800, 800);
-    angleMode(DEGREES);
-    noStroke();
-    for (let i = 0; i < dotCount; i++) {
-        dots.push({ x: random(width), y: random(height), color: color(random(255), random(255), random(255), 100) });
-    }
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  frameRate(100);
+  
+  // Create 10,000 circles
+  for (let i = 0; i < 1000000; i++) {
+    circles.push({
+      x: random(width),
+      y: random(height),
+      size: random(10),
+      color: color(random(100,255), random(255), random(255)),
+      noiseOffsetX: random(1000),
+      noiseOffsetY: random(1000)
+    });
+  }
 }
 
 function draw() {
-    background('white');
-    for (let dot of dots) {
-        let angle = random(360);
-        let distance = random(10);
-        let dx = cos(angle) * distance;
-        let dy = sin(angle) * distance;
-        dot.x += dx;
-        dot.y += dy;
-        dot.x = constrain(dot.x, 0, width);
-        dot.y = constrain(dot.y, 0, height);
-        fill(dot.color);
-        ellipse(dot.x, dot.y, 1);
-    }
-}
 
+    let circle = circles[i];
+    
+
+    // Draw the circle
+    fill('blue');
+    text(i, x, y);
+    
+
+    if (i <= circles.length) {
+        i += 1;
+    }
+
+    if (x <= width) {
+        x += random(5);
+    } else {
+        x = 0;
+    }
+
+    if (y <= height) {
+        y += random(5);
+    } else {
+        y = 0;
+    }
+  
+}
