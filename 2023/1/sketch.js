5f00f8591a429e5d96898fb3546e2a9b792e6484
yukseltron <h.yuksel04@gmail.com>
2025-07-18 09:17:14 -0400

    refinement

--- 2023/1/sketch.js
+++ 2023/1/sketch.js
@@ -1,60 +1,38 @@
-
+let planets = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 20; i++) {
+    planets.push({
+      neg: random([1, -1]),
+      r: random(5, 100),
+      color: 'white',
+      revolution: random(50, 300),
+      steps: int(random(100, 600))
+    });
+  }
 }
 
-//-------------------------------------------
 function draw() {
-
-    background(0);
-
-
-    //planet2
-    planet(-1, 10, 'white', 10.0, 400)
-    planet(-1, 20, 'white', 20, 200)
-    planet(1, 80, 'white', 10.0, 600)
-
-
-    planet(1, 100, 'white', 1, 100)
-
-    planet(1, 15, '#ffffff', 150.0, 400)
-    planet(1, 200, 'white', 200.0, 400)
-    planet(1, 100, 'white', 150.0, 200)
-    planet(1, 300, 'white', 100.0, 200)
-
-    planet(1, 10, '#ffffff', 250.0, 600)
-    planet(1, 10, '#ffffff', 150.0, 200)
-    planet(1, 10, '#ffffff', 250.0, 200)
-
-    planet(1, 100, '#ffffff', 300.0, 600)
-    planet(1, 50, '#ffffff', 300.0, 400)
-
-    planet(1, 10, '#ffffff', 350.0, 600)
-    planet(1, 20, '#ffffff', 350.0, 400)
-
-    planet(1, 10, '#ffffff', 375.0, 200)
-
+  background(0);
+  for (let p of planets) {
+    planet(p.neg, p.r, p.color, p.revolution, p.steps);
+  }
 }
 
 function planet(neg, r, color, revolution, steps) {
-    var currStep = frameCount % steps;
-    var t = map(currStep, 0, steps, 0, TWO_PI);
-    var px = width / 2.0 + revolution * cos(t) * neg;
-    var py = height / 2.0 + revolution * sin(t);
+  var currStep = frameCount % steps;
+  var t = map(currStep, 0, steps, 0, TWO_PI);
+  var px = width / 2.0 + revolution * cos(t) * neg;
+  var py = height / 2.0 + revolution * sin(t);
 
-    noFill();
-    stroke(color);
-    ellipse(px, py, r, r);
+  noFill();
+  stroke(color);
+  ellipse(px, py, r, r);
 }
 
 function keyPressed() {
-  // this will download the first 5 seconds of the animation!
   if (key === 's') {
     saveGif('day1', 5);
   }
 }
-
-
-
-
--- 2023/4/sketch.js
+++ 2023/4/sketch.js
@@ -1,33 +1,30 @@
-
-let a;
+let intersections = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  a = height*2;
+  for (let i = 1; i <= 10; i++) {
+    intersections.push({
+      s: height * 2 / i,
+      color: [
+        random(0, 255),
+        random(0, 255),
+        random(0, 255)
+      ],
+      weight: random(1, 6),
+      decay: random(0.1, 1)
+    });
+  }
 }
 
 function draw() {
-  background(0);
-  drawIntersection(a);
-  drawIntersection(a/2);
-  drawIntersection(a/3);
-  drawIntersection(a/4);
-  drawIntersection(a/5);
-  drawIntersection(a/6);
-  drawIntersection(a/7);
-  drawIntersection(a/8);
-  drawIntersection(a/9);
-  drawIntersection(a/10);
-}
-
-function drawIntersection(s) {
   let color1 = random(0, 205);
   let color2 = random(0, 25);
   let color3 = random(0, 105);
-  stroke(color1, color2, color3);
-  
-  line(-s, -s, width, s*s);
-  strokeWeight(5);
-  
-  a = a - 0.5;
+  background(0);
+  for (let inter of intersections) {
+    stroke(color1, color2, color3);
+    strokeWeight(inter.weight);
+    line(-inter.s, -inter.s, width, inter.s * inter.s);
+    inter.s -= inter.decay;
+  }
 }
